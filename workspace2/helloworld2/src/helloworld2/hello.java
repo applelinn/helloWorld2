17605e836d441ec9e2cfe599ec123827d3d016ea
apple <apple@172.27.203.200>
2015-10-11 23:58:48 +0800

    hey b1

---
+++ workspace2/helloworld2/src/helloworld2/hello.java
@@ -0,0 +1,16 @@
+package helloworld2;
+
+public class hello {
+	
+	public static void main (String[] argv)
+	{
+		System.out.println("hey");
+		apple(4);
+	}
+	
+	public static void apple (int i)
+	{
+		System.out.println(i);
+	}
+
+}
